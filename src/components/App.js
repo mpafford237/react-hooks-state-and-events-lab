6a853e17e0c18e9452c48c0c10da95d7280c0c44
masonPafford <mdpafford2015@gmail.com>
2024-08-08 12:19:33 -0500

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  //In the `App` component, there is a button for toggling between dark mode and
+  //light mode. Using the `useState` hook, create a state variable in the `App`
+  //component. Then, use that variable to determine if our app should be in dark
+  //mode or light mode.
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  
+  //You will also need to add an event handler to the dark mode button, and update
+  //state when the button is clicked.
+
+  const handleDarkModeToggle = () => {
+    setIsDarkMode ((prevMode) => !prevMode);
+  }
+  
+  //The actual functionality of changing our app's theme is handled in CSS, so all
+  //you have to do is set up the code to update the `className` of the div based on
+  //your state variable:
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = isDarkMode ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDarkModeToggle}>Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
+//In the `Item` component, when the user clicks the `<button>` element, the item
+//should be added to their virtual cart. The way we'll show the user that the item
+//is in the cart is by changing the className on the `<li>` element:
+
+//```jsx
+//<li className="in-cart">
+//{/* the item is in the cart */}
+//</li>
+
+//<li className="">
+//{/* the item is NOT in the cart */}
+//</li>
+//```
+
+//If the item _is not_ in the cart, the `<button>` element's text should read "Add
+//to Cart", and if the item _is_ in the cart, the `<button>` element's text should
+//read "Remove From Cart". Naturally, you'll also need to add state to the `Item`
+//component to solve this deliverable!
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+  const handleCartToggle = () => {
+    setIsInCart((prevIsInCart) => !prevIsInCart)
+  };
+  const liClass = isInCart ? "in-cart" : "";
+  const buttonText = isInCart ? "Remove From Cart" : "Add to Cart";
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCartToggle}>{buttonText}</button>
     </li>
   );
 }
